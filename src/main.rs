0bf7d34fc0d169dbcd99ea728806c9922ea6c218
NotAShelf <itsashelf@gmail.com>
2023-02-21 00:11:27 +0300

    feat: version, help & init commands

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
 use std::env;
 
-// my modules
-use project::{new_project};
-//use lib::{help};
+// get the help function from lib.rs
+use sysdev::{help, version};
 
-// version number - TODO: automate this later
-const VERSION: &str = "0.0.1";
+pub mod project;
 
-// program name - for if I ever want to change it
-const NAME: &str = "sysdev";
+// get new_project from project/mod.rs Project impl
+use project::Project;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 1 {
+    if args.len() > 1 {
+        let command = &args[1];
+        match &command[..] {
+            "version" | "--version" | "-v" => version(),
+            "help" | "--help" | "-h" => help(),
+            "init" => Project::new_project(args),
 
-        // display the welcome banner enclosed in a box - TODO: make this look better because it looks ugly
-        println!("┌──────────────────────────────────────────────┐");
-        println!("│    Welcome to {} {}                   │", NAME, VERSION);
-        println!("│     A multi-purpose command-line interface   │");
-        println!("│     for system administration tasks.         │");
-        println!("└──────────────────────────────────────────────┘");
-
-    } else {
-        if args.len() > 1 {
-            let command = &args[1];
-            match &command[..] {
-                // "help" | "--help" | "-h" | _ => help(),
-                "version" | "--version" | "-v" => println!("{} {}", NAME, VERSION),
+            _ => {
+                println!("┌──────────────────────────────────────────────┐");
+                println!(
+                    "│    Welcome to {} {}                   │",
+                    sysdev::NAME,
+                    sysdev::VERSION
+                );
+                println!("│     A multi-purpose command-line interface   │");
+                println!("│     for system administration tasks.         │");
+                println!("└──────────────────────────────────────────────┘");
             }
         }
     }
