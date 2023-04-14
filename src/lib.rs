f7373e22afa1f815ff68d54985c4de4636d49929
phant80m <black.pandazayd@gmail.com>
2023-04-14 10:53:05 +1000

    color, centered objects, :)

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub fn help() {
              "[Project Management]".green(),
              "> init".purple(), "--name".red(), "[NAME]".green(), "--path".yellow(), "[PATH]".green(), "--lang".blue(), "[LANGUAGE]".green(), "--git".magenta(), "[GIT]".green(),
              "Initialize a new project with the given parameters
-           Example: fyg init --name \"My Project\" --lang \"Rust\" -path\"/home/user/projects/my-project\" --git".bright_red(),
+           Example: fyg init --name \"My Project\" --lang \"Rust\" --path\"/home/user/projects/my-project\" --git".bright_red(),
            "[System Management]".green(),
            "[Package Management]".green()
 
