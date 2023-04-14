be75f8962d72742b838f9c212b131964fa878904
phant80m <black.pandazayd@gmail.com>
2023-04-14 11:12:17 +1000

    added help cosntant back, centered init text

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
-use colored::Colorize;
 // get project info from Cargo.toml
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 
+// help message with ansii color codes
+const HELP_TEXT: &str = "\x1b[1;31mfylgja is a multi-purpose command-line interface for system administration tasks.\x1b[0m
+
+    \x1b[34mUsage: fyg [COMMAND] [ARGUMENTS]\x1b[0m
+    
+    \x1b[1;31mAvailable commands:\x1b[0m
+
+        \x1b[32m[Project Management]\x1b[0m
+
+        > \x1b[35minit\x1b[0m \x1b[31m--name\x1b[0m \x1b[32m[NAME]\x1b[0m \x1b[33m--path\x1b[0m \x1b[32m[PATH]\x1b[0m \x1b[34m--lang\x1b[0m \x1b[32m[LANGUAGE]\x1b[0m \x1b[35m--git\x1b[0m \x1b[32m[GIT]\x1b[0m 
+            Initialize a new project with the given parameters
+            Example: fyg init --name \"My Project\" --lang \"Rust\" --path\"/home/user/projects/my-project\" --git
+
+        \x1b[32m[System Management]\x1b[0m
+
+        \x1b[32m[Package Management]\x1b[0m";
+
 pub fn help() {
-
-    println!("{}\n\n    {} {} {} {}\n\n    {}\n\n       {}\n\n        {} {} {} {} {} {} {} {} {}\n\n           {}\n\n       {}\n\n       {}",
-             "fylgja is a multi-purpose command-line interface for system administration tasks.".bold().red(),
-             "Usage:", "fyg".blue(), "[COMMAND]".purple(), "[ARGUMENTS]".bright_cyan(),
-             "Available commands:".bright_red(),
-             "[Project Management]".green(),
-             "> init".purple(), "--name".red(), "[NAME]".green(), "--path".yellow(), "[PATH]".green(), "--lang".blue(), "[LANGUAGE]".green(), "--git".magenta(), "[GIT]".green(),
-             "Initialize a new project with the given parameters
-           Example: fyg init --name \"My Project\" --lang \"Rust\" --path\"/home/user/projects/my-project\" --git".bright_red(),
-           "[System Management]".green(),
-           "[Package Management]".green()
-
-             )
+    println!("{}", HELP_TEXT);
 }
 
 pub fn version() {
