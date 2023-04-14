e4b86692b7410cecbe8f6bc49b397325a34befd6
phant80m <black.pandazayd@gmail.com>
2023-04-14 10:49:56 +1000

    color, and re-aranged and aligned mesages

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 // get project info from Cargo.toml
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const NAME: &str = env!("CARGO_PKG_NAME");
@@ -10,7 +11,8 @@ const SYSDEV_HELP: &str = "fylgja is a multi-purpose command-line interface for 
 
         [Project Management]
 
-        > init --name [NAME] --path [PATH] --lang [LANGUAGE] --git [GIT] 
+        > init --name [NAME] --path [PATH] --lang [LANGUAGE] --git [GIT]
+
             Initialize a new project with the given parameters
             Example: fyg init --name \"My Project\" --lang \"Rust\" -path\"/home/user/projects/my-project\" --git
 
@@ -20,8 +22,21 @@ const SYSDEV_HELP: &str = "fylgja is a multi-purpose command-line interface for 
 
 ";
 
+
 pub fn help() {
-    println!("{}", SYSDEV_HELP);
+
+    println!("{}\n\n    {} {} {} {}\n\n    {}\n\n       {}\n\n        {} {} {} {} {} {} {} {} {}\n\n           {}\n\n       {}\n\n       {}",
+             "fylgja is a multi-purpose command-line interface for system administration tasks.".bold().red(),
+             "Usage:", "fyg".blue(), "[COMMAND]".purple(), "[ARGUMENTS]".bright_cyan(),
+             "Available commands:".bright_red(),
+             "[Project Management]".green(),
+             "> init".purple(), "--name".red(), "[NAME]".green(), "--path".yellow(), "[PATH]".green(), "--lang".blue(), "[LANGUAGE]".green(), "--git".magenta(), "[GIT]".green(),
+             "Initialize a new project with the given parameters
+           Example: fyg init --name \"My Project\" --lang \"Rust\" -path\"/home/user/projects/my-project\" --git".bright_red(),
+           "[System Management]".green(),
+           "[Package Management]".green()
+
+             )
 }
 
 pub fn version() {
